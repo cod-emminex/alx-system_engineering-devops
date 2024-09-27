acdc782f661dcaf8b5266c8c32224d41952e47e4
cod-emminex <emminexy@yahoo.com>
2024-09-27 23:22:48 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 import requests
-
-
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
-    with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
-        try:
-            return info.get('data', {}).get('subscribers', 0)
-        except Exception:
-            return 0
+    """
+    Queries the Reddit API to retrieve the number of subscribers for a given subreddit.
+
+    Args:
+    subreddit (str): The name of the subreddit to query.
+
+    Returns:
+    int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
